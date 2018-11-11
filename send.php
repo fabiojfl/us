637aee282f877327dc80db50477c4f04c3d93bf4
Fabiojs <fabiojfl@gmail.com>
2018-11-11 14:04:24 -0200

    start

---
+++ send.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+// multiple recipients
+$to  = 'contato@usinadesons.com.br' . ', '; // note the comma
+$to .= 'usinaproducoeseeventos@gmail.com';
+
+// subject
+$subject = $_POST['subject'];
+
+// message
+$text    = $_POST['message'];
+$name    = $_POST['name'];
+$toMail  = $_POST['email'];
+
+$message = "<b>Mensagem do Site da Usina de Sons <em>usinadesons.com.br</em>&nbsp;</b></br><b>&nbsp;Nome:&nbsp; </b>".$name."</br>"."<b>&nbsp;Email: &nbsp;</b>".$toMail."</br>"."<b>&nbsp;Mensagem: &nbsp;</b>".$text."</br>";
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+
+// Mail it
+mail($to, $subject, $message, $headers);
+
+
+?>
+<script>
+ location.href="contact2.html"; 
+</script>
---
+++ sendTel.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+// multiple recipients
+$to  = 'contato@usinadesons.com.br' . ', '; // note the comma
+$to .= 'usinaproducoeseeventos@gmail.com';
+
+$name      = $_POST['name'];
+$telefone  = $_POST['telefone'];
+
+// subject
+$subject = "Ligue para:".$name;
+
+// message
+$message = $telefone;
+
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+
+// Mail it
+mail($to, $subject, $message, $headers);
+
+
+?>
+<script>
+ location.href="contact-tel2.html"; 
+</script>
